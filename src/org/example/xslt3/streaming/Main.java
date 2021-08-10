e0f253fd013a288889f5d69285abf361e09e91c8
Martin Honnen <martin.honnen@gmx.de>
2021-08-10 09:21:21 +0200

    Initial commit

---
+++ src/org/example/xslt3/streaming/Main.java
@@ -0,0 +1,22 @@
+package org.example.xslt3.streaming;
+
+import net.sf.saxon.s9api.*;
+
+import javax.xml.transform.stream.StreamSource;
+
+public class Main {
+
+    public static void main(String[] args) throws SaxonApiException {
+        Processor processor = new Processor(true);
+
+        XsltCompiler compiler = processor.newXsltCompiler();
+
+        XsltExecutable executable = compiler.compile(new StreamSource("count-example1.xsl"));
+
+        Xslt30Transformer transformer = executable.load30();
+
+        XdmAtomicValue result = (XdmAtomicValue)transformer.applyTemplates(new StreamSource("sample1.xml"));
+
+        System.out.println(result.getDoubleValue());
+    }
+}
